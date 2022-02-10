5da1044427cf793de0c7daf138da641da3d4e873
Victor <vzabrovskiu@gmail.com>
2022-02-10 21:25:21 +0300

    more structure

---
+++ web/src/features/Landing/Landing.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react'
+import { style, Wrapper } from './styles'
+
+const LandingPage = (): ReactElement => {
+    return (
+        <Wrapper>
+        </Wrapper>
+    )
+}
+
+export default LandingPage

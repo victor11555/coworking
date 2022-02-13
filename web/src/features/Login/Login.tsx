28a70bdc5e63792ed009deb97f10eb0ccd7ac5ed
Victor <vzabrovskiu@gmail.com>
2022-02-13 10:53:34 +0300

    10: delete card usage

--- web/src/features/Login/Login.tsx
+++ web/src/features/Login/Login.tsx
@@ -1,12 +1,10 @@
 import React, { ReactElement } from 'react'
 import { Wrapper, style } from './styles'
-import Card from '../../ui-library/components/Card'
 
 const Login = (): ReactElement => {
     return (
         <Wrapper>
-            {/*<div>Login</div>*/}
-            <Card>login</Card>
+            <div>Login</div>
         </Wrapper>
     )
 }
